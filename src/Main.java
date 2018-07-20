79001cebbd136b4de9a6576421e2d92d47577194
real-primarchvg <primarchvg@gmail.com>
2018-07-20 17:53:16 +0300

    finish mod.4 lesson 4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,64 @@
+import io.hexlet.xo.model.*;
+import io.hexlet.xo.model.Point;
+import io.hexlet.xo.model.exceptions.*;
+import io.hexlet.xo.view.*;
+import io.hexlet.xo.controllers.*;
+
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String... args) {
+		final Field field = new Field();
+		final CurrentMoveController cmc = new CurrentMoveController();
+		ConsoleView print = new ConsoleView();
+
 		final Point p = new Point();
-		p.x = 2;
-		p.y = 3;
+		p.x = 1;
+		p.y = 1;
 
-		final Player player = new Player();
-		player.name = "Michael";
-		player.figure = "X";
+		if (!cmc.currentMove(field).equals("X")){
+			throw new RuntimeException("do not returns X when the field is empty");
+		}
+		final TempTest tt = new TempTest();
+
+		for (int i = 0; i < field.getSize(); i++) 
+			for (int i2 = 0; i2 < field.getSize(); i2++) {
+				p.x = i;
+				p.y = i2;
+				final String figure = (i * 3 + i2) % 2 == 0 ? "X" : "O";
+				field.setFigure(p, figure);
+				//System.out.printf("putting figure: %s to the: X:%d Y:%d\n", figure, p.x, p.y);
+				if (cmc.currentMove(field).equals(figure)){
+					throw new RuntimeException(String.format("returns %s for the field: \n%s", figure, print.show(field)));
+				}
+			}
+		
+		tt.checkField(null, null, null, null, null, null, null, null, null, null);
+		//одинаковые поля блоками. Разница в заподнении по строкам или по столбцам.
+		// для понимания, что нет в этом разницы
+		tt.checkField(null, "X", "O", "X", "X", "O", "O", "O", "X", "X");
+		tt.checkField(null, "X", "X", "O", "O", "O", "X", "X", "O", "X");
+
+		tt.checkField("X", "X", "X", "X", null, "O", null, "O", null, null);//или по строкам или по столбцам
+		tt.checkField("X", "X", null, "O", "X", "O", null, "X", null, null);//или по строкам или по столбцам
+
+		tt.checkField("O", "X", null, "X", "O", "O", "O", "X", null, null);
+		tt.checkField("O", "X", "O", "X", null, "O", null, "X", "O", null);
+
+		tt.checkField("X", null, "X", null, null, "X", null, null, "X", null);
+		tt.checkField("X", null, null, null, "X", "X", "X", null, null, null);
+
+		tt.checkField("O", null, null, "O", null, null, "O", null, null, "O");
+		tt.checkField("O", null, null, null, null, null, null, "O", "O", "O");
+
+		tt.checkField("O", "O", "X", "X", null, "O", null, "X", null, "O");
+		tt.checkField("O", "O", null, "X", "X", "O", null, "X", null, "O");
+
+		tt.checkField("X", "X", "O", "X", "O", "X", null, "X", null, "O");//эти две строки хорошо демонстрируют,
+		tt.checkField("X", "X", "O", "X", "O", "X", null, "X", null, "O");//что не важно какая координата будет х, а какая у
+
+		tt.checkField("O", "X", null, "O", "X", "O", "X", "O", null, "O");
+		tt.checkField("O", "X", "X", "O", null, "O", null, "O", "X", "O");
 	}
-
 }
--- src/io/hexlet/xo/controllers/WinnerController.java
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -1,13 +1,92 @@
 package io.hexlet.xo.controllers;
-// BEGIN (write your solution here)
+
 import io.hexlet.xo.model.Field;
-// END
+import io.hexlet.xo.model.Point;
+
 public class WinnerController {
-	// BEGIN (write your solution here)
-	public String getWinner(Field field) { // возвращает победителя (если имеется)
 
-		return "X";
+    // BEGIN (write your solution here) простое решение
+	public String getWinner(Field field) {
+		final Point p1 = new Point();
+		final Point p2 = new Point();
+		final Point p3 = new Point();
+		p1.x = 0;
+		p2.x = 1;
+		p3.x = 2;
 
+		p1.y = 0;
+		p2.y = 1;
+		p3.y = 2;
+
+		if (field.getFigure(p1) != null &&
+			field.getFigure(p2) != null &&
+			field.getFigure(p3) != null &&
+			field.getFigure(p1).equals(field.getFigure(p2)) &&
+			field.getFigure(p1).equals(field.getFigure(p3))) {
+
+			return field.getFigure(p1);
+		}
+
+		p1.x = 2;
+		p2.x = 1;
+		p3.x = 0;
+
+		p1.y = 0;
+		p2.y = 1;
+		p3.y = 2;
+
+		if (field.getFigure(p1) != null &&
+			field.getFigure(p2) != null
+			&& field.getFigure(p3) != null &&
+			field.getFigure(p1).equals(field.getFigure(p2)) &&
+			field.getFigure(p1).equals(field.getFigure(p3))) {
+
+			return field.getFigure(p1);
+		}
+
+		for (int i = 0; i < field.getSize(); i++) {
+			p1.x = 0;
+			p2.x = 1;
+			p3.x = 2;
+
+			p1.y = i;
+			p2.y = i;
+			p3.y = i;
+
+			if (field.getFigure(p1) != null && field.getFigure(p2) != null
+					&& field.getFigure(p3) != null &&
+					field.getFigure(p1).equals(field.getFigure(p2)) &&
+					field.getFigure(p1).equals(field.getFigure(p3))) {
+						return field.getFigure(p1);
+					}
+		}
+
+		for (int i = 0; i < field.getSize(); i++) {
+			p1.x = i;
+			p2.x = i;
+			p3.x = i;
+
+			p1.y = 0;
+			p2.y = 1;
+			p3.y = 2;
+
+			if (field.getFigure(p1) != null &&
+				field.getFigure(p2) != null &&
+				field.getFigure(p3) != null &&
+				field.getFigure(p1).equals(field.getFigure(p2)) &&
+				field.getFigure(p1).equals(field.getFigure(p3))) {
+
+				return field.getFigure(p1);
+			}
+		}
+
+		return null;
 	}
 	// END
+
+	// BEGIN (write your solution here) или решение для квадратного поля любой размерности.
+
+	// END
 }
+
+
